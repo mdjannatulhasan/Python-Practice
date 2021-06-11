6f42db8f9e1b5aa90d5191f44a9158be2852bc2f
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2021-06-11 20:02:10 +0600

    1

---
+++ Day 1/test.py
@@ -0,0 +1,12 @@
+print("Hello world")
+print('*' * 9)
+print("*" * 9)
+# PProgram 1
+# personName = input('What is your name? ' )
+# fvColour  = input('What is your favourite color? ' )
+# print("Hello, " + personName + '. '+ "Your favourite color is "+fvColour)
+
+# Program 2
+kg = input('Enter you weight ')
+pound = float(kg) * 2.20462
+print("Your weight is "+ str(pound) +" pounds")
